384fc0da93964f5078066118a28a1a0b9830d833
nebulaone20 <115531985+nebulaone20@users.noreply.github.com>
2026-06-29 19:38:37 +1000

    Update preload-overlay.js

--- src/preload-overlay.js
+++ src/preload-overlay.js
@@ -4,4 +4,7 @@ contextBridge.exposeInMainWorld('bridge', {
   onHealthUpdate: (callback) => {
     ipcRenderer.on('health-update', (event, data) => callback(data));
   },
+  onAgentUpdate: (callback) => {
+    ipcRenderer.on('agent-update', (event, data) => callback(data));
+  },
 });
